0bea0d907852cdc50bf71fabb30c6a71981bd951
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-26 00:27:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { useAuthStore } from "./store/authStore";
 
+function App() {
+  const user = useAuthStore((state) => state.user);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={user ? <Dashboard /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
