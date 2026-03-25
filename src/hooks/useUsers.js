9587b08fe364b193f70f2df9ae89a9416498c1f8
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-26 00:25:23 +0530

    Update useUsers.js

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,1 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchUsers } from "../services/api";
 
+export const useUsers = () => {
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+};
